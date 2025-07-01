3c44dcbc2cb156d3044a4f19923492a7004cdd70
Ignacio Ortego <ignacioortego110@gmail.com>
2025-07-01 11:26:00 -0300

    listen.erl documentado

--- listen.erl
+++ listen.erl
@@ -1,11 +1,11 @@
 -module(listen).
 -export([start/1]).
 
--define(PORT, 12346).
-
+%% Inicia el loop
 start(Socket) ->
     loop(Socket).
 
+%% Filtra los mensajes UDP que no son de sí mismo y los handlea
 loop(Socket) ->
     MyIP = nodo:get_my_ip(),
     case gen_udp:recv(Socket, 0) of
@@ -21,12 +21,14 @@ loop(Socket) ->
             loop(Socket)
     end.
 
+%% Handlea los distintos mensajes UDP
 handle_message(Msg, Host, Socket) ->
     case string:tokens(string:trim(Msg), " \n") of
+        %% Si recibe HELLO, agrega o actualiza al map de nodos conocidos
         ["HELLO", NodeId, TcpPort] ->
             knownNodes ! {new, NodeId, list_to_integer(TcpPort), Host},
             loop(Socket);
-
+        %% Si recibe NAME_REQUEST, chequea que no exista en nodos conocidos y que sea distinto a su propio id
         ["NAME_REQUEST", ReqId] ->
             knownNodes ! {exists, ReqId, self()},
             receive
@@ -50,6 +52,7 @@ handle_message(Msg, Host, Socket) ->
             loop(Socket)
     end.
 
+%% Envia un INVALID_NAME <nodo_id> al nodo con un name request incorrecto por unicast
 send_invalid_name(Socket, Host, NodeId) ->
     Msg = io_lib:format("INVALID_NAME ~s\n", [NodeId]),
     io:format("Enviando mensaje: ~s a ~p~n", [Msg, Host]),
