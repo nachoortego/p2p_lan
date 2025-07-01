e18786657cca5c7a35a0671c3022ca1999a60339
Sofia Grepachok <grepachoks@gmail.com>
2025-07-01 11:03:45 -0300

    arreglo es_archivo

--- listar_archivos.erl
+++ listar_archivos.erl
@@ -1,7 +1,9 @@
 -module(listar_archivos).
 -export([listar/1]).
+
 -include_lib("kernel/include/file.hrl").
 
+%% Lista los nombres de archivos en el directorio dado.
 listar(Directorio) ->
     case file:list_dir(Directorio) of
         {ok, Archivos} -> 
@@ -10,8 +12,14 @@ listar(Directorio) ->
             {error, "No se pudo acceder al directorio"}
     end.
 
+%% Verifica si una entrada del directorio es un archivo regular.
 es_archivo(Directorio, Archivo) ->
-    case file:read_file_info(filename:join(Directorio, Archivo)) of
-        {ok, #file_info{type = regular}} -> true;
+    Path = filename:join(Directorio, Archivo),
+    case file:read_file_info(Path) of
+        {ok, Info} ->
+            case Info#file_info.type of
+                regular -> true;
+                _ -> false
+            end;
         _ -> false
     end.
